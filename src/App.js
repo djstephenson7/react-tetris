03e87145efeaef327ab01709382890d51d7ad2c4
Daniel Stephenson <daniel@giffgaff.co.uk>
2019-08-16 16:48:52 +0100

    Basic components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 import Tetris from "./components/tetris";
 
-const App = () => (
-  <div className="App">
-    <Tetris />
-  </div>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Tetris />
+      </div>
+    );
+  }
+}
+
+export default App;
