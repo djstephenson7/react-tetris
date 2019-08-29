e3e46339b93ab1a7653bcb6ee22818bfea684a12
Daniel Stephenson <daniel@giffgaff.co.uk>
2019-08-29 09:41:03 +0100

    Fixed start button

--- src/components/common/startButton.jsx
+++ src/components/common/startButton.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { StyledStartButton } from "../styles/styledStartButton";
 
-const StartButton = () => <StyledStartButton>Start Game</StyledStartButton>;
+const StartButton = ({ callback }) => (
+  <StyledStartButton onClick={callback}>Start Game</StyledStartButton>
+);
 
 export default StartButton;
